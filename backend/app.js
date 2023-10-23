4bb964bab0c48f2a1d23065cac86f377807d0f1c
Jatin Singhi <singhijatin02@gmail.com>
2023-10-23 15:31:34 +0530

    First commit

---
+++ backend/app.js
@@ -0,0 +1,24 @@
+const express = require("express"); 
+const app = express();
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+var cors = require('cors');
+
+// database connection
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+}).then(() => console.log("DB connected"))
+    .catch((err) => console.log(err));
+
+
+//port
+const port = process.env.PORT || 8000
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
