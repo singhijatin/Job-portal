d8607032afa612c10076aa8ef3cc36bda37822c8
Jatin Singhi <singhijatin02@gmail.com>
2023-10-23 16:38:08 +0530

    2nd par

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,11 @@ const morgan = require("morgan");
 const bodyParser = require("body-parser");
 require("dotenv").config();
 var cors = require('cors');
+const cookieParser = require("cookie-parser");
+const errorHandler = require("./middleware/error");
+
+
+
 
 // database connection
 mongoose.connect(process.env.DATABASE, {
@@ -15,6 +20,19 @@ mongoose.connect(process.env.DATABASE, {
 }).then(() => console.log("DB connected"))
     .catch((err) => console.log(err));
 
+
+//Middleware
+app.use(morgan('dev'));
+app.use(bodyParser.json({limit: "5mb"}));
+app.use(bodyParser.urlencoded({
+    limit: "5mb",
+    extended:true
+}));
+app.use(cookieParser());
+app.use(cors());
+
+//error middleware
+app.use(errorHandler);
 
 //port
 const port = process.env.PORT || 8000
